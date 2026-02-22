979b70363fb609d731d51e6df69d53eda4c84572
Jon Reid <jon@qualitycoding.org>
2026-02-21 17:31:23 -1000

    . e Make test.sh quiet on success to reduce context

--- test.sh
+++ test.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Running OutputTracking tests..."
-swift test
 
-echo "Running ExampleApp tests..."
-cd ExampleApp
-swift test
+TMP_OUTPUT=$(mktemp)
 
-echo "All tests passed!"
+echo -n "Running OutputTracking tests… "
+if swift test  > "$TMP_OUTPUT" 2>&1; then
+  echo "passed."
+else
+  echo ""
+  cat "$TMP_OUTPUT"
+  echo "❌ OutputTracking tests failed."
+  rm "$TMP_OUTPUT"
+  exit 1
+fi
+
+echo -n "Running ExampleApp tests… "
+if cd ExampleApp; swift test  > "$TMP_OUTPUT" 2>&1; then
+  echo "passed."
+else
+  echo ""
+  cat "$TMP_OUTPUT"
+  echo "❌ ExampleApp tests failed."
+  rm "$TMP_OUTPUT"
+  exit 1
+fi
+
+rm "$TMP_OUTPUT"
+echo "✅ All tests passed"
